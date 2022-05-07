9a4f724f59a0766eef0d013621ff5279d84591c2
Thomas Aurelius <45251987+ThomasAurelius@users.noreply.github.com>
2022-05-07 00:46:40 -0500

    working on pokecard

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import React from 'react'
 import Header from './Header'
 import PokeCard from './PokeCard'
 import './style.css';
 
 function App() {
-
   
+  const [pokemonData, setPokemonData] = React.useState(
+
+  React.useEffect(() => {
+    const getPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    
+    setPokemonData(data)
+    
+    
+  }
+    getPokemon(1)  
+    }, [])  
+  )
+
+
+
   return (
     <div className="App">
       <Header />
-      <PokeCard />
+      <PokeCard pokemonData={pokemonData} />
     </div>
   );
 }
--- src/PokeCard.js
+++ src/PokeCard.js
@@ -1,14 +1,22 @@
-
+import React from 'react'
 
 function PokeCard(props) {
-
-  const [data, setData] = React.useState(
-    
-  )
- 
+         
   return (
+    
     <div className="PokeCard">
-        
+      <div className="pokemon">
+        <div id={props.name}>
+           <div className="img-container">
+             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="" />
+           </div>
+           <div className="info">
+             <span className="number">#{props.id}</span>
+             <h3 className="name">{props.name}</h3>
+             <small className="type">Type: <span>{}</span></small>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
